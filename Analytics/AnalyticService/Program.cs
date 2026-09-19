f07a5e67d4ff2d4805aaa93e990e72b34bb24008
Luis Fernando Alvarez <lfal328@gmail.com>
2026-09-19 18:00:56 -0500

    Adicionar Metricas y Endpoint

---
+++ Analytics/AnalyticService/Program.cs
@@ -0,0 +1,58 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("AnalyticsService"))
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri("http://otel-collector:4317");
+            });
+    })
+    .WithMetrics(metricsProviderBuilder =>
+    {
+        metricsProviderBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri("http://otel-collector:4317");
+            });
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger en desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Endpoint GET de simulación
+app.MapGet("/ping", () =>
+{
+    return Results.Ok(new
+    {
+        message = "pong",
+        service = "AnalyticsService",   // 🔹 cambia este nombre según el microservicio
+        timestamp = DateTime.UtcNow
+    });
+})
+.WithName("PingService");
+
+app.Run();
---
+++ Users/UserService/Program.cs
@@ -0,0 +1,41 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de OpenTelemetry
+builder.Services.AddSingleton<TracerProvider>(_ =>
+    Sdk.CreateTracerProviderBuilder()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://otel-collector:4317");
+        })
+        .Build());
+
+builder.Services.AddSingleton<MeterProvider>(_ =>
+    Sdk.CreateMeterProviderBuilder()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://otel-collector:4317");
+        })
+        .Build());
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+// Endpoint GET de simulación
+app.MapGet("/ping", () =>
+{
+    return Results.Ok(new { message = "pong", service = "UserService", timestamp = DateTime.UtcNow });
+})
+.WithName("PingService");
+
+app.Run();
