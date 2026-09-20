584454aebdc85c86dd2d33cc8db649cc79143efd
Luis Fernando Alvarez <lfal328@gmail.com>
2026-09-20 02:16:36 -0500

    Actualizar

--- Inventory/InventoryService/obj/Debug/net8.0/InventoryService.AssemblyInfo.cs
+++ Inventory/InventoryService/obj/Debug/net8.0/InventoryService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0905e850125cf0049a536a9222657327f9a3285c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740262b10bc3044d20d39af9381a15a605ba627f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
