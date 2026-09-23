36f15bdf2a3ab7c14cc9318198ee04e97c848e4a
Luis Fernando Alvarez <lfal328@gmail.com>
2026-09-23 14:32:39 -0500

    Add

--- check-services.ps1
+++ check-services.ps1
@@ -1,17 +1,17 @@
 ﻿# Lista de servicios con sus puertos
 $services = @{
-    users = 5001
-    products = 5002
-    orders = 5003
-    payments = 5004
-    inventory = 5005
-    shipping = 5006
-    cart = 5007
-    reviews = 5008
-    auth = 5009
-    notifications = 5010
-    analytics = 5011
-    support = 5012
+    users = 5091
+    products = 5257
+    orders = 5059
+    payments = 5195
+    inventory = 5138
+    shipping = 5134
+    cart = 5151
+    reviews = 5146
+    auth = 5292
+    notifications = 5221
+    analytics = 5126
+    support = 5185
 }
 
 Write-Host "🔎 Validando microservicios..."
